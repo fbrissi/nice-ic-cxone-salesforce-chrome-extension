065d40b6342a9f5c45b34a0bdc8136af9f7a5283
Filipe Bojikian Rissi <f.b.rissi@gmail.com>
2020-09-15 10:46:53 -0300

    First commit

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[path][name]-[hash:8].[ext]',
+              name: '[path][name].[ext]',
             },
           },
         ],
